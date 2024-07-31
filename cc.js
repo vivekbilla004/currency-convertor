d9d6f8dcef702d7822dcd4ee9bd15502a3efba14
vivek <vivekbilla822@gmail.com>
2024-07-31 19:03:20 +0530

    Your commit message

---
+++ cc.js
@@ -0,0 +1,59 @@
+const BASE_URL = "https://api.exchangerate-api.com/v4/latest";
+
+// Select necessary DOM elements
+const dropdowns = document.querySelectorAll(".dropdown select");
+const btn = document.querySelector("form button");
+const fromCurr = document.querySelector(".from select");
+const toCurr = document.querySelector(".to select");
+const msg = document.querySelector(".msg");
+
+
+
+// Populate dropdowns with currency codes and set default selections
+for (let select of dropdowns) {
+  for (let currCode in countryList) {
+    let newOption = document.createElement("option");
+    newOption.innerText = currCode;
+    newOption.value = currCode;
+    if (select.name === "from" && currCode === "USD") {
+      newOption.selected = "selected";
+    } else if (select.name === "to" && currCode === "INR") {
+      newOption.selected = "selected";
+    }
+    select.append(newOption);
+  }
+
+  // Add change event listener to update the flag when selection changes
+  select.addEventListener("change", (evt) => {
+    updateFlag(evt.target);
+  });
+}
+
+// Function to update the country flag based on the selected currency
+const updateFlag = (element) => {
+  let currCode = element.value;
+  let countryCode = countryList[currCode];
+  let newSrc = `https://flagsapi.com/${countryCode}/flat/64.png`;
+  let img = element.parentElement.querySelector("img");
+  img.src = newSrc;
+};
+
+// Event listener for form submission to perform currency conversion
+btn.addEventListener("click", async (evt) => {
+  evt.preventDefault();
+  let amount = document.querySelector(".amount input");
+  let amtVal = amount.value;
+  if (amtVal === "" || amtVal < 1) {
+    amtVal = 1;
+    amount.value = "1";
+  }
+
+  // Fetch the exchange rate and calculate the converted amount
+  const URL = `${BASE_URL}/${fromCurr.value}`;
+  let response = await fetch(URL);
+  let data = await response.json();
+  let rate = data.rates[toCurr.value];
+
+  let finalAmount = amtVal * rate;
+  msg.innerText = `${amtVal} ${fromCurr.value} = ${finalAmount.toFixed(2)} ${toCurr.value}`;
+});
